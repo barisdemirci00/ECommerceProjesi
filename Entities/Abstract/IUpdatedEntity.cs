707aa6944f027cc923c85a7fa559468cb4a48020
BarisD <barisdemirci@gmail.com>
2022-06-26 15:07:50 +0300

    Entities Abstract da IUpdatedEntity oluşturuldu.

---
+++ Entities/Abstract/IUpdatedEntity.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Entities.Abstract
+{
+    public interface IUpdatedEntity
+    {
+        int? UpdatedUserId { get; set; }
+        DateTime? UpdatedTime { get; set; }
+    }
+}
