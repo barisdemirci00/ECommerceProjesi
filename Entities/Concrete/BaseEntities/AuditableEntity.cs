f49ae270c1e2fec337fc5a025ee51e0f3ea2191d
BarisD <barisdemirci@gmail.com>
2022-06-26 15:09:06 +0300

    Entities Concrete de AuditableEntity oluşturuldu.

---
+++ Entities/Concrete/BaseEntities/AuditableEntity.cs
@@ -0,0 +1,14 @@
+﻿using Entities.Abstract;
+using System;
+
+namespace Entities.Concrete.BaseEntities
+{
+    public class AuditableEntity : BaseEntity, ICreatedEntity, IUpdatedEntity
+    {
+        int ICreatedEntity.CreatedUserId { get; set; }
+        DateTime ICreatedEntity.CreatedTime { get; set; }
+        public int? UpdatedUserId { get ; set ; }
+        public DateTime? UpdatedTime { get; set; }
+       
+    }
+}
